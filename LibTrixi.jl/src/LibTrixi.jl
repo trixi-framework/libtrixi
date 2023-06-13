e2f3eb05009e82441f9ac36ea01ae7e9538f1a7f
Michael Schlottke-Lakemper <michael@sloede.com>
2023-06-13 15:16:09 +0200

    Add initial version of a LibTrixi.jl package

---
+++ LibTrixi.jl/src/LibTrixi.jl
@@ -0,0 +1,36 @@
+module LibTrixi
+
+export trixi_initialize, trixi_finalize, trixi_get_timestep, trixi_integrate,
+       trixi_eval_string
+
+Base.@ccallable function trixi_initialize(comm::Cint)::Cvoid
+    # Init Trixi
+    return nothing
+end
+const trixi_initialize_c = @cfunction(trixi_initialize, Cvoid, (Cint,))
+
+Base.@ccallable function trixi_finalize()::Cvoid
+    # Finalize Trixi
+    return nothing
+end
+const trixi_finalize_c = @cfunction(trixi_finalize, Cvoid, ())
+
+Base.@ccallable function trixi_get_timestep()::Cdouble
+    # Return time step size
+    return 1.0
+end
+const trixi_get_timestep_c = @cfunction(trixi_get_timestep, Cdouble, ())
+
+Base.@ccallable function trixi_integrate()::Cvoid
+    # Integrate in time for one time step
+    return nothing
+end
+const trixi_integrate_c = @cfunction(trixi_integrate, Cvoid, ())
+
+Base.@ccallable function trixi_eval_string(code::Cstring)::Cvoid
+    # Integrate in time for one time step
+    return nothing
+end
+const trixi_eval_string_c = @cfunction(trixi_eval_string, Cvoid, (Cstring,))
+
+end # module LibTrixi
