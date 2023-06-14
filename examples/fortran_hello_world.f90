fe599d4992851d3ef1a967263349a4701e2aaf55
Benedict Geihe <bgeihe@uni-koeln.de>
2023-06-14 10:20:18 +0200

    moved fortran module definition to new file

--- examples/fortran_hello_world.f90
+++ examples/fortran_hello_world.f90
@@ -1,23 +1,3 @@
-module LibTrixi
-  implicit none
-
-  interface
-    subroutine trixi_initialize(comm) bind(c)
-      use, intrinsic :: iso_c_binding, only: c_int
-      integer, intent(in) :: comm
-    end subroutine
-
-    subroutine trixi_finalize() bind(c)
-    end subroutine
-
-    subroutine julia_eval_repl(code) bind(c)
-      use, intrinsic :: iso_c_binding, only: c_char
-      character(kind=c_char) :: code(*)
-    end subroutine
-  end interface
-end module
-
-
 program fortran_hello_world
   use LibTrixi
 
