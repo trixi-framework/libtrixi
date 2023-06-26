a3fa5e466c0b7668963928de2f64f86a0358e982
bgeihe <135045760+bgeihe@users.noreply.github.com>
2023-06-26 18:14:47 +0200

    Document troubleshooting (#34)

--- docs/make.jl
+++ docs/make.jl
@@ -33,6 +33,7 @@ makedocs(
         "Reference" => [
                         "LibTrixi.jl" => "reference-libtrixi-jl.md",
                        ],
+        "Troubleshooting" => "troubleshooting.md",
         "License" => "license.md"
     ],
     strict = true # to make the GitHub action fail when doctests fail, see https://github.com/neuropsychology/Psycho.jl/issues/34
