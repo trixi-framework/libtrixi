9154e70b765abf9c108ada7caa014070d76c5864
Benedict Geihe <bgeihe@uni-koeln.de>
2023-07-06 16:32:09 +0200

    link against t8code

--- cmake/FindT8code.cmake
+++ cmake/FindT8code.cmake
@@ -13,6 +13,8 @@ endif()
 find_path ( T8CODE_INCLUDE_DIR t8.h PATHS ${T8CODE_PREFIX}/include )
 find_program ( T8CODE_VERSION_EXE t8_version PATHS ${T8CODE_PREFIX}/bin )
 find_library ( T8CODE_LIBRARY t8 PATHS ${T8CODE_PREFIX}/lib )
+find_library ( P4EST_LIBRARY p4est PATHS ${T8CODE_PREFIX}/lib )
+find_library ( SC_LIBRARY sc PATHS ${T8CODE_PREFIX}/lib )
 
 
 
@@ -37,6 +39,8 @@ endif ()
 include ( FindPackageHandleStandardArgs )
 find_package_handle_standard_args(
     T8code
-    REQUIRED_VARS   T8CODE_LIBRARY T8CODE_INCLUDE_DIR
+    REQUIRED_VARS   T8CODE_LIBRARY SC_LIBRARY P4EST_LIBRARY T8CODE_INCLUDE_DIR
     VERSION_VAR     T8CODE_VERSION_STRING
 )
+
+set ( T8CODE_LIBRARIES ${T8CODE_LIBRARY} ${P4EST_LIBRARY} ${SC_LIBRARY})
