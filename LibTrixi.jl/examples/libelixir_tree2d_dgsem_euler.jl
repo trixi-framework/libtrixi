155e895362888a77a8b2cebe89e6cecc43bf88da
Benedict Geihe <bgeihe@uni-koeln.de>
2023-07-14 16:02:48 +0200

    set refinement level to 2 for testing

--- LibTrixi.jl/examples/libelixir_tree2d_dgsem_euler.jl
+++ LibTrixi.jl/examples/libelixir_tree2d_dgsem_euler.jl
@@ -28,7 +28,7 @@ function init_simstate()
     coordinates_max = (2.0, 2.0)
 
     mesh = TreeMesh(coordinates_min, coordinates_max,
-                    initial_refinement_level=4,
+                    initial_refinement_level=2,
                     n_cells_max=10_000)
 
     semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
