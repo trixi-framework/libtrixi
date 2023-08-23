123a1877c0994905db0ff4e7ddf62d34c309ccfe
Benedict Geihe <bgeihe@uni-koeln.de>
2023-08-23 15:23:25 +0200

    test show_debug_output()

--- LibTrixi.jl/test/runtests.jl
+++ LibTrixi.jl/test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 
 @time @testset verbose=true showtiming=true "libtrixi tests" begin
+    include("test_auxiliary.jl")
     include("test_function_pointers.jl")
     include("test_interface.jl")
 end
