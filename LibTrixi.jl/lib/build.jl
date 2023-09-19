8d6f2a759b5bfc6f9c5f178333bfa8a7112af988
Michael Schlottke-Lakemper <michael@sloede.com>
2023-09-19 18:26:01 +0200

    Clean up build script

--- LibTrixi.jl/lib/build.jl
+++ LibTrixi.jl/lib/build.jl
@@ -5,26 +5,25 @@ using PackageCompiler: PackageCompiler
 using TOML: TOML
 
 lib_dir = @__DIR__
-package_dir = dirname(lib_dir)
 
-if length(ARGS) < 1 || "-h" in ARGS || "--help" in ARGS
+if length(ARGS) < 2 || "-h" in ARGS || "--help" in ARGS
     project = relpath(lib_dir)
     println("""
-            usage: julia --project=$project $PROGRAM_FILE DEST_DIR
+            usage: julia --project=$project $PROGRAM_FILE PACKAGE_OR_PROJECT_DIR DEST_DIR
 
             Build libtrixi as a library directly from the Julia sources using PackageCompiler.jl.
 
             positional arguments:
+                PACKAGE_OR_PROJECT_DIR
+                                Path to the package or project directory that should be compiled.
                 DEST_DIR        Directory where the library bundle should be created.
             """)
     exit(1)
 end
-dest_dir = ARGS[1]
+package_or_project_dir = ARGS[1]
+dest_dir = ARGS[2]
 
 lib_name = "trixi"
-
-# precompile_execution_file = joinpath(lib_dir, "precompile_execution_file.jl")
-precompile_execution_file = ""
 
 incremental = true
 
@@ -34,44 +33,41 @@ force = true
 
 header_files = [joinpath(dirname(dirname(lib_dir)), "src", "trixi.h")]
 
-# julia_init_c_file = ["init.c", PackageCompiler.default_julia_init()]
-julia_init_c_file = "init-project.c"
-
-# julia_init_h_file = [PackageCompiler.default_julia_init_header()]
+julia_init_c_file = "init.c"
 
 project_toml = realpath(joinpath(dirname(lib_dir), "Project.toml"))
 version = VersionNumber(TOML.parsefile(project_toml)["version"])
 
 compat_level = "minor"
 
+include_lazy_artifacts = true
+
 include_transitive_dependencies = true
 
 @info("List of arguments passed to `create_library`:",
-      package_dir,
+      package_or_project_dir,
       dest_dir,
       lib_name,
-      precompile_execution_file,
       incremental,
       filter_stdlibs,
       force,
       header_files,
       julia_init_c_file,
-      # julia_init_h_file,
       version,
       compat_level,
+      include_lazy_artifacts,
       include_transitive_dependencies)
 
-total_time = @elapsed PackageCompiler.create_library(package_dir, dest_dir;
+total_time = @elapsed PackageCompiler.create_library(package_or_project_dir, dest_dir;
                                                      lib_name,
-                                                     precompile_execution_file,
                                                      incremental,
                                                      filter_stdlibs,
                                                      force,
                                                      header_files,
                                                      julia_init_c_file,
-                                                     # julia_init_h_file,
                                                      version,
                                                      compat_level,
+                                                     include_lazy_artifacts,
                                                      include_transitive_dependencies)
 
 @info "Total time (in seconds)" total_time
