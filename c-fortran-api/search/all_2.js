fb844a649ed8e203f01f9480dbe5d7bae42a0d42
Documenter.jl <documenter@juliadocs.github.io>
2024-10-14 07:58:22 +0000

    delete history

---
+++ c-fortran-api/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['julia_5feval_5fstring_2',['julia_eval_string',['../group__api__c.html#gaf26e7c57d1b64df4958c88ba26bbf3a4',1,'julia_eval_string(const char *code):&#160;trixi.c'],['../group__api__c.html#gaf26e7c57d1b64df4958c88ba26bbf3a4',1,'julia_eval_string(const char *code):&#160;trixi.c'],['../group__api__f.html#ga358b1eb93a3ad82fdffacc06b01747e7',1,'libtrixi::julia_eval_string()']]],
+  ['julia_5feval_5fstring_5fc_3',['julia_eval_string_c',['../group__api__f.html#ga02374b59baa2f97f753b86458f1d33aa',1,'libtrixi::julia_eval_string_c::julia_eval_string_c()'],['../interfacelibtrixi_1_1julia__eval__string__c.html',1,'julia_eval_string_c']]]
+];
---
+++ previews/PR72/c-fortran-api/search/classes_1.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['trixi_5fcalculate_5fdt_33',['trixi_calculate_dt',['../interfacelibtrixi_1_1trixi__calculate__dt.html',1,'libtrixi']]],
+  ['trixi_5ffinalize_34',['trixi_finalize',['../interfacelibtrixi_1_1trixi__finalize.html',1,'libtrixi']]],
+  ['trixi_5ffinalize_5fsimulation_35',['trixi_finalize_simulation',['../interfacelibtrixi_1_1trixi__finalize__simulation.html',1,'libtrixi']]],
+  ['trixi_5finitialize_5fc_36',['trixi_initialize_c',['../interfacelibtrixi_1_1trixi__initialize__c.html',1,'libtrixi']]],
+  ['trixi_5finitialize_5fsimulation_5fc_37',['trixi_initialize_simulation_c',['../interfacelibtrixi_1_1trixi__initialize__simulation__c.html',1,'libtrixi']]],
+  ['trixi_5fis_5ffinished_5fc_38',['trixi_is_finished_c',['../interfacelibtrixi_1_1trixi__is__finished__c.html',1,'libtrixi']]],
+  ['trixi_5fstep_39',['trixi_step',['../interfacelibtrixi_1_1trixi__step.html',1,'libtrixi']]],
+  ['trixi_5fversion_5fc_40',['trixi_version_c',['../interfacelibtrixi_1_1trixi__version__c.html',1,'libtrixi']]],
+  ['trixi_5fversion_5fmajor_41',['trixi_version_major',['../interfacelibtrixi_1_1trixi__version__major.html',1,'libtrixi']]],
+  ['trixi_5fversion_5fminor_42',['trixi_version_minor',['../interfacelibtrixi_1_1trixi__version__minor.html',1,'libtrixi']]],
+  ['trixi_5fversion_5fpatch_43',['trixi_version_patch',['../interfacelibtrixi_1_1trixi__version__patch.html',1,'libtrixi']]]
+];
