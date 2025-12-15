83d251a99dc31f8585fd557e52660ae4dfc3a3af
Documenter.jl <documenter@juliadocs.github.io>
2025-12-15 13:55:54 +0000

    delete history

---
+++ c-fortran-api/search/enumvalues_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['trixi_5fftpr_5fcalculate_5fdt_51',['TRIXI_FTPR_CALCULATE_DT',['../trixi_8c.html#a06fc87d81c62e9abb8790b6e5713c55ba43046ff5ca493f50f8ff9c961bdd7254',1,'trixi.c']]],
+  ['trixi_5fftpr_5ffinalize_5fsimulation_52',['TRIXI_FTPR_FINALIZE_SIMULATION',['../trixi_8c.html#a06fc87d81c62e9abb8790b6e5713c55ba051772f320c405880111d77ce89bbe89',1,'trixi.c']]],
+  ['trixi_5fftpr_5finitialize_5fsimulation_53',['TRIXI_FTPR_INITIALIZE_SIMULATION',['../trixi_8c.html#a06fc87d81c62e9abb8790b6e5713c55ba5aa94cf378760f52f7b563529b3b2f45',1,'trixi.c']]],
+  ['trixi_5fftpr_5fis_5ffinished_54',['TRIXI_FTPR_IS_FINISHED',['../trixi_8c.html#a06fc87d81c62e9abb8790b6e5713c55ba1b372835a8c38d65a37a0ccd29b9f83f',1,'trixi.c']]],
+  ['trixi_5fftpr_5fstep_55',['TRIXI_FTPR_STEP',['../trixi_8c.html#a06fc87d81c62e9abb8790b6e5713c55ba6c5bcab9fddf65f9d1aeb2dabbe18f0d',1,'trixi.c']]],
+  ['trixi_5fnum_5ffptrs_56',['TRIXI_NUM_FPTRS',['../trixi_8c.html#a06fc87d81c62e9abb8790b6e5713c55bac0ad0ec60970da8e15799e07816772ed',1,'trixi.c']]]
+];
