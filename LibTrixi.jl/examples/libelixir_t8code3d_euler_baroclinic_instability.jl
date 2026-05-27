2469d502ae26b1953a35e619faaade75c8ac9166
Benedict Geihe <bgeihe@uni-koeln.de>
2026-05-27 12:21:42 +0200

    adapt calc_sources

--- LibTrixi.jl/examples/libelixir_t8code3d_euler_baroclinic_instability.jl
+++ LibTrixi.jl/examples/libelixir_t8code3d_euler_baroclinic_instability.jl
@@ -22,7 +22,7 @@ struct SourceTerm
 end
 
 # We overwrite Trixi.jl's internal method here such that it calls source_terms with indices
-function Trixi.calc_sources!(du, u, t, source_terms::SourceTerm,
+function Trixi.calc_sources!(backend::Nothing, du, u, t, source_terms::SourceTerm,
                              equations::CompressibleEulerEquations3D, dg::DG, cache)
     @unpack node_coordinates = cache.elements
     Trixi.@threaded for element in eachelement(dg, cache)
