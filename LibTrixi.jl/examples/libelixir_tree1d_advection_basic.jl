82232903cd427bba7b4a6516989e3304dffd41af
Benedict Geihe <bgeihe@uni-koeln.de>
2026-09-11 18:23:13 +0200

    remove n_cells_max

--- LibTrixi.jl/examples/libelixir_tree1d_advection_basic.jl
+++ LibTrixi.jl/examples/libelixir_tree1d_advection_basic.jl
@@ -20,7 +20,7 @@ function init_simstate()
     # Create a uniformly refined mesh with periodic boundaries
     mesh = TreeMesh(coordinates_min, coordinates_max,
                     initial_refinement_level = 4,
-                    n_cells_max = 30_000, periodicity = true)
+                    periodicity = true)
 
     # A semidiscretization collects data structures and functions for the spatial discretization
     semi = SemidiscretizationHyperbolic(mesh, equations,
